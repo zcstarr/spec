4e2da7203770e0ad979d0c87eee40e18c4638113
Zane Starr <zcstarr@gmail.com>
2026-02-12 19:59:53 -0800

    fix: add support for a mapping to return that mapping of schemas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import OpenRPCSpecificationSchema1_4 from "../spec/1.4/schema.json";
 import OpenRPCSpecificationSchemaLegacy from "../spec/legacy/schema.json";
 export { OpenRPCSpecificationSchema1_4, OpenRPCSpecificationSchemaLegacy };
+export const getAllSchemas = () =>{
+  return {
+  "1.4": OpenRPCSpecificationSchema1_4, 
+  "legacy": OpenRPCSpecificationSchemaLegacy,
+  }
+};
